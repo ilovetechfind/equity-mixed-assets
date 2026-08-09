1992797fc717f2530d04084053b53503e15e78f4
ilovetechfind <ilovetechfind@gmail.com>
2026-08-09 18:26:39 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://rrddtmqojyheshznrwl.supabase.co';
+const SUPABASE_ANON_KEY = 'Sb_publishable_0fS5eYtsQL8tZz67eSWDvQ_G4flOw5M';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
