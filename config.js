0fcd5a513313ff8e64a0e7861f5edf246d0803a1
ilovetechfind <ilovetechfind@gmail.com>
2026-08-12 18:04:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const SUPABASE_URL = "https://rrdddtmqojyheshznrwl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZGRkdG1xb2p5aGVzaHpucndsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODUxNjEsImV4cCI6MjEwMTg2MTE2MX0.0Gp4-6D-TKBMgvHYZhKncfyfcWRTGd2oeSzpJGUg8vQ";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Ensures supabase is initialized safely
+const { createClient } = window.supabase;
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
